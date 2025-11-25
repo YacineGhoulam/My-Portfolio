f156dbe599ba1f751b3c7bc1d6b8686390111fde
Yacine Ghoulam <yacineghoulam@gmail.com>
2025-11-25 22:56:51 +0100

    added contacts

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,47 @@ function App() {
 						<h2 class="text-3xl font-semibold my-6">
 							Ingénieur en Informatique
 						</h2>
+						<div class="mt-12 flex flex-col items-center">
+							<span class="text-lg font-semibold mb-6">
+								Trouvez moi sur:
+							</span>
+							<div class="flex space-x-12">
+								<a
+									href="https://github.com/YacineGhoulam"
+									target="_blank"
+									rel="noopener noreferrer"
+									class="flex items-center hover:text-light transition"
+								>
+									{/* GitHub Icon */}
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										class="h-6 w-6 mr-2"
+										fill="currentColor"
+										viewBox="0 0 24 24"
+									>
+										<path d="M12 2C6.477 2 2 6.484 2 12.012c0 4.418 2.865 8.166 6.839 9.489.5.092.682-.217.682-.482 0-.237-.009-.868-.014-1.703-2.782.605-3.369-1.342-3.369-1.342-.454-1.155-1.11-1.463-1.11-1.463-.908-.62.069-.608.069-.608 1.004.07 1.532 1.032 1.532 1.032.892 1.53 2.341 1.088 2.91.832.091-.646.35-1.088.636-1.339-2.221-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.254-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.025A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.338 1.909-1.295 2.748-1.025 2.748-1.025.546 1.378.202 2.396.1 2.65.64.7 1.028 1.595 1.028 2.688 0 3.847-2.337 4.695-4.566 4.944.359.309.678.919.678 1.852 0 1.336-.012 2.417-.012 2.747 0 .267.18.578.688.48C19.138 20.175 22 16.427 22 12.012 22 6.484 17.523 2 12 2z" />
+									</svg>
+									GitHub
+								</a>
+								<a
+									href="https://www.linkedin.com/in/ghlamallah-yacine/"
+									target="_blank"
+									rel="noopener noreferrer"
+									class="flex items-center hover:text-light transition"
+								>
+									{/* LinkedIn Icon */}
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										class="h-6 w-6 mr-2"
+										fill="currentColor"
+										viewBox="0 0 24 24"
+									>
+										<path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.761 0 5-2.239 5-5v-14c0-2.761-2.239-5-5-5zm-11 19h-3v-10h3v10zm-1.5-11.268c-.966 0-1.75-.784-1.75-1.75s.784-1.75 1.75-1.75 1.75.784 1.75 1.75-.784 1.75-1.75 1.75zm13.5 11.268h-3v-5.604c0-1.337-.026-3.063-1.868-3.063-1.868 0-2.156 1.459-2.156 2.967v5.7h-3v-10h2.881v1.367h.041c.401-.761 1.379-1.563 2.838-1.563 3.036 0 3.6 2 3.6 4.594v5.602z" />
+									</svg>
+									LinkedIn
+								</a>
+							</div>
+						</div>
 					</div>
 				</div>
 				<div class="lg:w-1/2 w-full h-screen lg:overflow-y-auto no-scrollbar">
@@ -311,6 +352,7 @@ function App() {
 								</ul>
 							</div>
 						</div>
+
 						{/* Download Resume Button */}
 						<div class="p-8 lg:w-3/5 w-full flex flex-col justify-left items-start">
 							<a
